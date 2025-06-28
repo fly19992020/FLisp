eb9a9595aefde12aa87311a40c6cbc24a739729a
fly19992020 <93079590+fly19992020@users.noreply.github.com>
2025-06-28 20:30:13 +0800

    added a test

--- FLisp/FLisp.cpp
+++ FLisp/FLisp.cpp
@@ -2,6 +2,7 @@
 #include "type.h"
 #include "list_spliting.h"
 #include "running.h"
+#include "vm.h"
 
 
 int main()
@@ -17,5 +18,18 @@ int main()
 	// 	return Flisp_Value();
 	// 	});
 	// Flisp_running("(print \"Hello, world!\")");
+	Flisp_VM vm = Flisp_VM();
+	Flisp_Func f = Flisp_Func();
+	f.set_func([](std::list<Flisp_Value> args_list) {
+		for (auto & i : args_list) {
+			std::string s;
+			i.get_value(s);
+			std::cout << s << " ";
+		}
+		std::cout << std::endl;
+		return Flisp_Value();
+	});
+	vm.add_function("print", f);
+	vm.running("(print \"Hello, world!\")");
 	return 0;
 }
