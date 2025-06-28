7df4b2ee4a58edd9abd9eeee1cacddb435719d0b
fly19992020 <93079590+fly19992020@users.noreply.github.com>
2025-06-28 20:25:02 +0800

    added the vm

--- FLisp/FLisp.cpp
+++ FLisp/FLisp.cpp
@@ -7,15 +7,15 @@
 int main()
 {
 	Flisp_Value x;
-	Flisp_functions["print"].set_func([](std::list<Flisp_Value> args_list) {
-		for (auto i = args_list.begin(); i != args_list.end(); i++) {
-			std::string s;
-			i->get_value(s);
-			std::cout << s << " ";
-		}
-		std::cout << std::endl;
-		return Flisp_Value();
-		});
-	Flisp_running("(print \"Hello, world!\")");
+	// Flisp_functions["print"].set_func([](std::list<Flisp_Value> args_list) {
+	// 	for (auto i = args_list.begin(); i != args_list.end(); i++) {
+	// 		std::string s;
+	// 		i->get_value(s);
+	// 		std::cout << s << " ";
+	// 	}
+	// 	std::cout << std::endl;
+	// 	return Flisp_Value();
+	// 	});
+	// Flisp_running("(print \"Hello, world!\")");
 	return 0;
 }
